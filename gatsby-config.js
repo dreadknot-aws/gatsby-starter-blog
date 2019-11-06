dafc2a24db7c390e44c0b1f84a286c8e72091d70
dreadknot-aws <57420214+dreadknot-aws@users.noreply.github.com>
2019-11-05 16:01:40 -0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     description: `A blog for an Old Goat`,
     siteUrl: `https://myoldgoat.com`,
     social: {
-      instagram: `myoldgoat`,
+      instagram: `myoldgoatog`,
     },
   },
   plugins: [
