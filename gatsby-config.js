d84097bdb9a8b85c117da98643ff5b9ad24bf34c
Dreadknot <dreadknot@gmail.com>
2020-01-02 14:41:15 -0800

    change title and name

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,15 +52,15 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-155199051-1`,
       },
     },
     `gatsby-plugin-feed`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `MyOldGoat has a blog`,
+        short_name: `myoldgoat`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
