6cd5aae1bb9ccdbcd2f3475dd982a09f20544b4c
Dreadknot <57420214+dreadknot-aws@users.noreply.github.com>
2020-05-20 21:25:22 -0700

    Update contact.js
    try to center

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,7 +3,10 @@ import { Link } from "gatsby"
 import Header from "../components/header"
 
 export default () => (
-  <div style={{ color: `teal` }}>
+  <div style={{ 
+    color: `teal`,
+    justify-content: center
+  }}>
     <Link to="/">Home</Link>
     <Header headerText="Contact" />
     <p>Send us a message!</p>myoldgoatog@gmail.com
